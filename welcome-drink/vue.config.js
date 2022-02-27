3e7f5f6067b3454ef821ded1b7fb427f2132c880
Supergrammer <supergrammer@nhn.com>
2022-02-27 17:21:17 +0900

    [Chore] 프로젝트 기본 설정 추가, #1

--- welcome-drink/vue.config.js
+++ welcome-drink/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    port: process.env.VUE_APP_PORT ? process.env.VUE_APP_PORT : 8080
+  },
   transpileDependencies: [
     'vuetify'
   ]
